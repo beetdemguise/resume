13035608181e7028497dc7d17fd0e4381d568bd6
Darin Douglass <ddouglass@barracuda.com>
2021-03-25 15:52:45 -0400

    set viewport size

--- bin/export-pdf.js
+++ bin/export-pdf.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 const convert = async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080});
     await page.goto('file:///Users/ddouglass/src/yaboi/resources/public/index.html', {waitUntil: 'networkidle2'});
     await page.pdf({path: 'resume.pdf', format: 'A4'});
     await browser.close();
