09a444d9ab5e3c222604edc544d416147268d2c4
Darin Douglass <ddouglass@barracuda.com>
2021-03-28 14:51:42 -0400

    fix hard-coded path in pdf generation

--- bin/export-pdf.js
+++ bin/export-pdf.js
@@ -1,9 +1,14 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
+
+const html_file = path.join(__dirname, "..", "index.html")
+
+console.log(`Rendering ${html_file}`)
 
 const convert = async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
-    await page.goto('file:///Users/ddouglass/src/yaboi/resources/public/index.html', {waitUntil: 'networkidle2'});
+    await page.goto(`file://${html_file}`, {waitUntil: 'networkidle2'});
     await page.emulateMedia('screen');
     await page.pdf({path: 'resume.pdf',
                     printBackground: true,
